ff50d6b78a2b1547ff746baaefed2413273aec7a
kholius <evanverite@hotmail.fr>
2021-10-08 02:02:36 +0200

    Fonction IP OK // Fonction SET Hostname OK // Prépa Rapport x2 // next step > Bundle + template rapport

--- Ansible_Master.sh
+++ Ansible_Master.sh
@@ -34,26 +34,65 @@ echo " for now, it's work only with apt!"
 update(){
     sudo apt update -y && sudo apt upgrade -y 
 }
+
 gst_hostnem(){
     read -p "Wich Hostname would you set ?" hostnem
     echo $hostnem
     $hostnem > /etc/hostname
     cat /etc/hostname
+    rapport_change_hostnem=$(cat /etc/hostname)
 }
+
+# Set IP fix
 ip_fix(){
+    
+    # Display Info
     sudo apt install net-tools -y 
-    ip a
-    read -p "" int_
-    read -p "" ip_
-    read -p "" msk_
-    read -p "" gtw_
-    read -p "" nemsrv_
-    read -p "" sts
+    ifconfig -s 
+    # Set var with Questions
+    echo " Well, it's time to set up your Network Interface " 
+    echo "[ Take care how your input done ]"
+    read -p " Wich Interface would you set ? : " int_
+    read -p " and Wich IP? : " ip_
+    read -p " well and wich netmask? [no CIDR]" msk_
+    read -p " Ok, all packet will pass by wich door [GTW]" gtw_
+    read -p " hmmm... have you a DNS? " nemsrv_
+    read -p " I suppose this Int will be up however I question you : " status_
+
+    # Display ALL conf Int and Routes
+    ifconfig -s
+    route
+    # Setting IP fix and Add route
+    ifconfig -v $int_ $ip_ netmask $msk_ $status_ # ifconfig  -v ens33 192.168.1.199 netmask 255.255.255.0 up
+    route add $gtw_ $int # route add default 192.168.1.240 ens33
+    # Get Output for Rapport
+    ifconfig $int_ | grep inet > result_change_ip.txt
+    route | grep $int > result_gtw_for_int.txt
+    # Print Result to User
+    echo " Change Apply... "
+    route | grep $int_
+    ifconfig $int_
+    echo " ...Done "
+
+
+    rapport_change_ip=$(cat result_change_ip.txt)
+    rapport_gtw_for_int=$(cat result_gtw_for_int.txt)
+
 
 
 }
-bundle(){
 
+bundle(){
+    echo " Install Bundle..."
+    sudo apt install vim -y
+    sudo apt install screenfetch -y
+    sudo apt install cockpit -y
+    sudo apt install ufw -y
+    sudo apt install open-ssh-server -y 
+    sudo apt install nmap -y 
+    sudo apt update -y
+    sudo apt upgrade -y
+    echo " Bundle Done. "
 }
 
 
@@ -69,4 +108,7 @@ bare(){
     # Gestion ifconfig
     ip_fix
     # Bundle
+    bundle
 }
+
+bare
